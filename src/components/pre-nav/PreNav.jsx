0edcc92ef5f54b4be6c8359c2595bd635af8d8e8
UmidjonShamsutdinov <umid.2825@gmail.com>
2024-11-10 06:34:20 +0500

    hero banner changed

--- src/components/pre-nav/PreNav.jsx
+++ src/components/pre-nav/PreNav.jsx
@@ -12,16 +12,16 @@ const PreNav = () => {
     <div className='preNav'>
         <Container>
             <div className="preNav__main">
-                <Link to={"/"}>
+                <Link to={"https://www.hp.com/us-en/workstations/workstation-pcs.html?jumpid=va_d08cf5d452"}>
                     <img src={logo1} alt="z-HP"/>                
                 </Link>
-                <Link to={"/"}>
+                <Link to={"https://www.omen.com/us/en.html?jumpid=va_d08b17a763"}>
                     <img src={logo2} alt="omen"/>
                 </Link>
-                <Link to={"/"}>
+                <Link to={"https://www.hp.com/us-en/poly.html?jumpid=va_ww_mu_mk_hc_r12129_aw_x_10957"}>
                     <img src={logo4} alt="hyper"/>
                 </Link>
-                <Link to={"/"}>
+                <Link to={"https://hyperx.com/?jumpid=va_d08fc7f0c6"}>
                     <img src={logo3} alt="poly"/>
                 </Link>
             </div>
