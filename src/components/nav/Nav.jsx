1996c81b26bda0876185865dc76b3a0793f5c330
UmidjonShamsutdinov <umid.2825@gmail.com>
2024-11-10 20:55:59 +0500

    navbar menus changed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.scss"
 import { Container } from '../../utils/Utils'
 import hpLogo from "../../assets/HP.png"
 import { Link } from 'react-router-dom'
 import navData from "../../db/data.json"
 import { IoIosSearch, IoMdHeartEmpty } from "react-icons/io";
-import { IoCartOutline } from "react-icons/io5";
+import { IoCartOutline, IoPerson } from "react-icons/io5";
 import { RxHamburgerMenu } from 'react-icons/rx'
+import { CiSquareRemove } from 'react-icons/ci'
 
 
 const Nav = () => {
+    const [hamburger, setHamburger] = useState(false)
+    const [commits, setCommits] = useState("flex")
+
+    const activeHamburger = ()=>{
+        setHamburger(!hamburger)
+        if(commits === "flex"){
+            setCommits("none")
+            console.log(123);            
+        }
+        else{
+            setCommits("flex")
+            console.log(456);            
+        }        
+    }
+    
   return (
     <div className='nav'>
         <Container>
@@ -29,7 +45,7 @@ const Nav = () => {
                         </ul>
                     </div>                
                 </div>
-                <div className="nav__commits">
+                <div className="nav__commits" style={{display: `${commits}`}}>
                     <div className="nav__search">
                         <input type="text" placeholder='Search HP.com'/>
                         <button type='button'><IoIosSearch/></button>
@@ -37,8 +53,10 @@ const Nav = () => {
                     <IoIosSearch className='mobile-search'/>
                     <Link to={"/"} className='cart'><IoCartOutline/></Link>
                     <Link to={"/"} className='signIn'>Sign In</Link>
-                    <RxHamburgerMenu className='menu__hamburger'/>
+                    <Link to={"/"} className='signIn-person'><IoPerson/></Link>
+                    <RxHamburgerMenu className={`menu__hamburger menu__hamburger${hamburger}`} onClick={activeHamburger}/>
                 </div>
+                <CiSquareRemove className='menu__remove' style={{display: `${commits === 'flex' ? "none" : "block"}`}} onClick={activeHamburger}/>
             </div>
         </Container>
     </div>
